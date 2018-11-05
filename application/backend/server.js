e677e5c8586773fd48f15227ae18c842e3dc8a4c
Niki <nikichetotam@gmail.com>
2018-11-05 16:39:50 +0300

    Connect client and server

--- application/backend/server.js
+++ application/backend/server.js
@@ -1,22 +1,23 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const Logger = require('koa-logger')
-const Cors = require('@koa/cors')
-const BodyParser = require('koa-bodyparser')
-const Helmet = require('koa-helmet')
-const respond = require('koa-respond')
+const Koa = require('koa');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Cors = require('@koa/cors');
+const BodyParser = require('koa-bodyparser');
+const Helmet = require('koa-helmet');
+const respond = require('koa-respond');
 const mongoose = require('mongoose'); 
+const koaStatic = require('koa-static');
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
-app.use(Helmet())
+app.use(Helmet());
 
 if (process.env.NODE_ENV === 'development') {
-  app.use(Logger())
+  app.use(Logger());
 }
 
-app.use(Cors())
+app.use(Cors());
 app.use(BodyParser({
   enableTypes: ['json'],
   jsonLimit: '5mb',
@@ -24,15 +25,16 @@ app.use(BodyParser({
   onerror: function (err, ctx) {
     ctx.throw('body parse error', 422)
   }
-}))
+}));
 
-app.use(respond())
+app.use(respond());
 
 // API routes
-require('./routes')(router)
-app.use(router.routes())
-app.use(router.allowedMethods())
+require('./routes')(router);
+app.use(router.routes());
+app.use(koaStatic('./build'));
+app.use(router.allowedMethods());
 
 mongoose.connect('mongodb://testUser:testUser123456789@ds253243.mlab.com:53243/finance-control');
 
-module.exports = app
+module.exports = app;
---
+++ application/frontend/src/components/investments/investments.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Investments extends Component {
+    render() {
+        return (
+            <div>Initial frontend</div>
+        );
+    }
+}
+
+export default Investments;
